6e9b6bc40edf41edfc3160df9e0c98f0232a012a
instaprospector3-web <instaprospector3@gmail.com>
2025-10-19 13:45:58 -0300

    Responsividade

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex items-center justify-center relative overflow-hidden pt-20"
+      className="min-h-screen flex items-center justify-center relative overflow-hidden pt-20 pb-20"
     >
       {/* Hero background image */}
       <img
